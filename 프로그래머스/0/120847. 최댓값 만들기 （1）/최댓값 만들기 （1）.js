6991b24d4aa6f0f118efb6f2d7ada4fc47c9e181
mlnwns <129190157+mlnwns@users.noreply.github.com>
2025-07-16 12:44:50 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let max = 0;
+    
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = i+1; j < numbers.length; j++){
+            if(numbers[i] * numbers[j] > max) max = numbers[i] * numbers[j];
+        }
+    }
+    return max;
+}
