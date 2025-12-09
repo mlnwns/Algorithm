426b48d8817739aa16cd2c4cd3b5ae29788c10b7
곽민준 <129190157+mlnwns@users.noreply.github.com>
2025-12-09 16:18:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,21 @@
+function solution(s) {
+  const number = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  
+  for (const [key, value] of Object.entries(number)) {
+    s = s.replaceAll(key, value);
+  }
+
+  return Number(s);
+}
