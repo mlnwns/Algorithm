684c1b3157b87f105d5dd24bbadfad42b8b64173
곽민준 <129190157+mlnwns@users.noreply.github.com>
2025-12-25 17:02:01 +0900

    [level 1] Title: 소수 만들기, Time: 19.35 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function isPrime(num) {
+	if (num < 2) return false;
+	for (let i = 2; i * i <= num; i++) {
+		if (num % i === 0) return false;
+	}
+	return true;
+}
+
+function solution(nums) {
+	let count = 0;
+
+	for (let i = 0; i < nums.length - 2; i++) {
+		for (let j = i + 1; j < nums.length - 1; j++) {
+			for (let k = j + 1; k < nums.length; k++) {
+				if (isPrime(nums[i] + nums[j] + nums[k])) count++;
+			}
+		}
+	}
+
+	return count;
+}
