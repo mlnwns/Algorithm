322a4b2f9a0c6ad6342249a9786baa863eee40e5
곽민준 <129190157+mlnwns@users.noreply.github.com>
2025-12-29 16:31:55 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,17 @@
+function solution(new_id) {
+	new_id = new_id.toLowerCase();
+	new_id = new_id.replace(/[^a-z0-9\-_.]/g, "");
+	new_id = new_id.replace(/\.+/g, ".");
+	new_id = new_id.replace(/^\.|\.&/g, "");
+	if (new_id === "") new_id += "a";
+	if (new_id.length >= 16) {
+		new_id = new_id.slice(0, 15);
+	}
+	new_id = new_id.replace(/\.$/g, "");
+
+	while (new_id.length <= 2) {
+		new_id += new_id[new_id.length - 1];
+	}
+    
+    return new_id;
+}
