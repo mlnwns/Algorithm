2483d4101dbf0cc1a02a4b913eb8af228be56d4a
곽민준 <129190157+mlnwns@users.noreply.github.com>
2026-01-14 21:46:27 +0900

    [level 2] Title: 카펫, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,13 @@
+function solution(brown, yellow) {
+  const total = brown + yellow;
+
+  for (let height = 3; height <= Math.sqrt(total); height++) {
+    if (total % height !== 0) continue;
+
+    const width = total / height;
+
+    if ((width - 2) * (height - 2) === yellow) {
+      return [width, height];
+    }
+  }
+}
